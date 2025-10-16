30603060325c9ac3adb77393c1c410be88cc9fda
KeshivT <keshivshaan@gmail.com>
2025-10-16 16:51:54 -0700

    polished code and added readme

--- interpreterv1.py
+++ interpreterv1.py
@@ -23,25 +23,27 @@ class Interpreter(InterpreterBase):
             return None
         
         functions = ast.get('functions')
+
         if functions is None:
             return None
-        
         for func in functions:
             if func.elem_type == InterpreterBase.FUNC_NODE:
-                    func_name = func.get('name')
-                    if func_name == 'main':
-                            return func
+                func_name = func.get('name')
+                if func_name == 'main':
+                    return func
+                else:
+                    super().error(ErrorType.NAME_ERROR, "No main() function was found")
                     
         return None
 
     def run_func(self, func_node):
-        # Also mostly copied from pseudocode, added evaluate for no statements
+        # Also mostly copied from pseudocode, added check for no statements
         # Go through statements in the function and run them
         statements = func_node.get('statements')
         if statements is None:
-              return
+            return
         for statement_node in statements:
-           self.run_statement(statement_node)
+            self.run_statement(statement_node)
 
 
     # Statement Nodes
@@ -56,31 +58,23 @@ class Interpreter(InterpreterBase):
             self.do_func_call(statement_node)
         else:
             super().error(ErrorType.TYPE_ERROR, f"Unknown statement type: {statement_node.elem_type}")
-    
-    # Check variable name
-    def var_name_exists(self, var_name):
-        return var_name in self.variable_name_to_value
-
-    # Get variable name
-    def get_target_variable_name(self, statement_node):
-        return statement_node.get('var')
         
     # Variable Definition Statement
     def do_definition(self, statement_node):
         var_name = statement_node.get('name')
-        if self.var_name_exists(var_name):
+        if var_name in self.variable_name_to_value:
             super().error(ErrorType.NAME_ERROR, f"Vairable {var_name} already defined")
         self.variable_name_to_value[var_name] = None
        
     # Assignemnt Statement
     def do_assignment(self, statement_node):
         # Credit to pseudocode!
-        var_name = self.get_target_variable_name(statement_node)
-        if not self.var_name_exists(var_name):
-            super().error(ErrorType.NAME_ERROR, f"Variable {var_name} not defined")
+        target_var_name = statement_node.get('name')
+        if not target_var_name in self.variable_name_to_value:
+            super().error(ErrorType.NAME_ERROR, f"Variable {target_var_name} not defined")
         source_node = statement_node.get('expression')
-        val = self.evaluate_expression(source_node)
-        self.variable_name_to_value[var_name] = val
+        resulting_value = self.evaluate_expression(source_node)
+        self.variable_name_to_value[target_var_name] = resulting_value
 
     # Function Call Statment
     def do_func_call(self, statement_node):
@@ -99,12 +93,14 @@ class Interpreter(InterpreterBase):
     def do_inputi(self, args):
         if args is None:
             args = []
-        if len(args) > 1:
-            super().error(ErrorType.NAME_ERROR, "No inputi() function found that takes > 1 parameter")
-        else:
-            ## 1 argument, which is correct
+
+        # Needs to be separate if statement
+        if len(args) == 1:
             prompt = self.evaluate_expression(args[0])
             super().output(str(prompt))
+        elif len(args) > 1:
+            super().error(ErrorType.NAME_ERROR, "No inputi() function found that takes > 1 parameter")
+        # Else would be no args, which is valid and nothing needs to happen
     
         input = super().get_input()
         return int(input)
@@ -144,6 +140,12 @@ class Interpreter(InterpreterBase):
     # Handles arithmetic
     def binary_operator(self, expression_node):
         op = expression_node.elem_type
+        # This splits the expression into two, then runs eval on both parts
+        # If both parts are binary operations in of themselves it basically recursively runs
+        # So (5 + 7) - (10 - 4) -> op1 = 5 + 7, op2 -> 10 - 4
+        # Then evaluate_expression calls binary_operator again on both ops and we get:
+        # op1 = 5, op2 = 7; op1 = 10, op2 = 4
+        # So 5 + 7 and 10 - 4 will be evaluated, then it'll subtract them
         op1 = self.evaluate_expression(expression_node.get('op1'))
         op2 = self.evaluate_expression(expression_node.get('op2'))
         
@@ -158,6 +160,9 @@ class Interpreter(InterpreterBase):
         elif op == '*':
             return op1 * op2
         elif op == '/':
+            if op2 == 0:
+                # Not really a fault error, but didn't want to change intbase to add new error type in case that messes with grading
+                super().error(ErrorType.FAULT_ERROR, "Cannot divide by 0. Fool.")
             return op1 / op2
         else:
             super().error(ErrorType.TYPE_ERROR, f"Unsupported operator: {op}")
@@ -176,8 +181,8 @@ class Interpreter(InterpreterBase):
     # Variable Expression
     def get_value_of_variable(self, expression_node):
         var_name = expression_node.get('name')
-        # Reading a declared-but-unassigned variable is an error
-        if not self.var_name_exists(var_name):
+        # Can't read an unassigned var
+        if not var_name in self.variable_name_to_value:
             super().error(ErrorType.NAME_ERROR, f"Variable {var_name} undefined")
         return self.variable_name_to_value[var_name]
 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Mostly just created Interpreter class and process all the AST nodes inside it using the provided Intbase.
+No known errors.
